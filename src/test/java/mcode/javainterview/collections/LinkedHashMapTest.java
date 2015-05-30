ab7426dc692d2ce1619eb744b5dfefef12dfeb92
bahboom <nongnongd@yahoo.com>
2015-05-29 21:08:23 -0400

    Fuck you. LinkedHashMap

---
+++ src/test/java/mcode/javainterview/collections/LinkedHashMapTest.java
@@ -0,0 +1,14 @@
+package mcode.javainterview.collections;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class LinkedHashMapTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
